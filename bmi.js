b2ea8532e7272f2220500fa8f364bbff8afb835d
duechayapolgmit <duechayapolsbs@gmail.com>
2022-09-30 11:10:49 +0100

    exercise 1

---
+++ bmi.js
@@ -0,0 +1,17 @@
+class BMI {
+    height;
+    weight;
+
+    constructor(height, weight){
+        this.height = height;
+        this.weight = weight;
+    }
+
+    calculateBMI(){
+        return this.weight/(this.height**2);
+    }
+}
+
+var bmiInstance = new BMI(1.74, 69);
+
+console.log(bmiInstance.calculateBMI());
