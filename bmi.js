026b4f6867d2e608a4ce90c529d5d6f428752ed2
duechayapolgmit <duechayapolsbs@gmail.com>
2022-09-30 12:14:42 +0100

    added comments

--- bmi.js
+++ bmi.js
@@ -1,14 +1,20 @@
+/**
+ * This class calculates the BMI of specified height and weight.
+ * Due - Chayapol Hongsrimuang - G00388741
+ */
 class BMI {
+    //constructor for the class
     constructor(height, weight) {
-        this.height = height;
+        this.height = height; //height in metres
         this.weight = weight;
     }
 
+    //calculates the BMI - weight / height ** 2
     calculateBMI() {
         return this.weight / (this.height ** 2);
     }
 }
 
+//testing - new object and invoking
 var bmiInstance = new BMI(1.74, 69);
-
 console.log(bmiInstance.calculateBMI());
--- cars.js
+++ cars.js
@@ -1,29 +1,42 @@
+/**
+ * These classes displays data of a Vehicle (make, model, year) and a Cars, extending functionality from Vehicle (make, model, year, doors)
+ * Due - Chayapol Hongsrimuang - G00388741
+ */
+
+//parent class - Vehicle
 class Vehicle {
+
+    //constructor for class
     constructor(make, model, year) {
         this.make = make;
         this.model = model;
         this.year = year;
     }
 
+    //logs model and year information in
     Information() {
         console.log(`Model: ${this.model}, Year: ${this.year}`);
     }
 }
 
+//children of Vehicle
 class Cars extends Vehicle {
+    //constructor for class
     constructor(make, model, year, doors) {
-        super(make, model, year);
+        super(make, model, year); //call parent's constructor
         this.doors = doors;
     }
 
+    //logs model, year, and doors information in
     Information() {
-        super.Information();
+        super.Information(); //call parent's Information() method
         console.log(`Doors ${this.doors}`);
     }
 }
 
-let bmtw = new Vehicle("a", "b", 1999);
+//testing - new objects and invoking
+let bmtw = new Vehicle("a", "b", 1999); //test for Vehicle
 bmtw.Information();
 
-let bmtw2 = new Cars("c", "d", 1998, 4);
+let bmtw2 = new Cars("c", "d", 1998, 4); //test for Cars
 bmtw2.Information();
